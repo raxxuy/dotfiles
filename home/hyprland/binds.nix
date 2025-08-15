d276c125192ba24b08312ca45889bb3361b09c9c
raxxuy <milanilkov@gmail.com>
2025-08-15 04:15:56 +0200

    feat: add new binds

--- home/hyprland/binds.nix
+++ home/hyprland/binds.nix
@@ -1,38 +1,49 @@
 let
-  inherit
-    (import ../../hosts/micho/variables.nix)
-    terminal
-    explorer
-    launcher
-    ;
+  inherit (import ../../hosts/micho/variables.nix) terminal explorer launcher;
+
+  mod = "$mod";
+
+  baseBinds = [
+    "${mod}, left, movefocus, l"
+    "${mod}, right, movefocus, r"
+    "${mod}, up, movefocus, u"
+    "${mod}, down, movefocus, d"
+    "${mod}, Q, exec, ${terminal}"
+    "${mod}, E, exec, ${explorer}"
+    "${mod}, R, exec, ${launcher}"
+    "${mod}, C, killactive"
+    "${mod}, F, togglefloating"
+    "${mod}, M, exit"
+    "${mod}, S, togglespecialworkspace, magic"
+    "${mod} SHIFT, S, movetoworkspace, special:magic"
+    "${mod}, A, exec, ags quit | ags run"
+    ",Print, exec, hyprshot -zm region --clipboard-only"
+  ];
+
+  workspaceBinds =
+    builtins.concatLists (
+      builtins.genList
+        (i:
+          let
+            ws = if i == 9 then 10 else (i + 1);
+            keyNum = if i == 9 then "0" else builtins.toString (i + 1);
+          in
+          [
+            "${mod}, ${keyNum}, workspace, ${builtins.toString ws}"
+            "${mod} SHIFT, ${keyNum}, movetoworkspace, ${builtins.toString ws}"
+          ]
+        )
+        10
+    );
+
+  mouseBinds = [
+    "${mod}, mouse:272, movewindow"
+    "${mod}, mouse:273, resizewindow"
+  ];
 in
 {
   wayland.windowManager.hyprland.settings = {
-    bind =
-      [
-        "$mod, Q, exec, ${terminal}"
-        "$mod, E, exec, ${explorer}"
-        "$mod, R, exec, ${launcher}"
-        "$mod, A, exec, ags quit | ags run"
-        "$mod, C, killactive"
-        "$mod, F, togglefloating"
-        "$mod, M, exit"
-        ",PRINT, exec, hyprshot -zm region --clipboard-only"
-      ]
-      ++ (
-        builtins.concatLists (
-          builtins.genList
-            (i:
-              let
-                ws = if i == 9 then 10 else (i + 1); # 0 key is last, workspace 10
-                keyNum = if i == 9 then "0" else builtins.toString (i + 1);
-              in
-              [
-                "$mod, ${keyNum}, workspace, ${builtins.toString ws}"
-                "$mod SHIFT, ${keyNum}, movetoworkspace, ${builtins.toString ws}"
-              ]
-            ) 10 # generate 10 entries: 0–9 index
-        )
-      );
+    bind = baseBinds ++ workspaceBinds;
+    bindm = mouseBinds;
   };
 }
