b2796b242b2fe067529625059196f465f985ca7c
raxxuy <milanilkov@gmail.com>
2025-09-24 19:08:08 +0200

    chore(git): add a credential manager

--- home/git.nix
+++ home/git.nix
@@ -1,7 +1,13 @@
+{ pkgs, ... }:
+
 let
   inherit (import ../hosts/micho/variables.nix) git;
 in
 {
+  home.packages = with pkgs; [
+    git-credential-manager
+  ];
+
   programs.git = {
     enable = true;
 
@@ -9,6 +15,10 @@ in
       user.name = git.name;
       user.email = git.email;
       init.defaultBranch = "main";
+
+      credential.helper = "manager";
+      credential."https://github.com".username = git.name;
+      credential.credentialStore = "cache";
     };
   };
 }
