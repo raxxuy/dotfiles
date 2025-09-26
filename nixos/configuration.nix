88ebeb1eba18e13135e925d331151967de38c777
raxxuy <milanilkov@gmail.com>
2025-09-26 15:12:30 +0200

    chore: switch to GRUB bootloader

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -6,7 +6,11 @@
   ];
 
   # Bootloader
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Enable networking
