fc32f14e36aa2ea4805ec501629337c5ccecebe8
raxxuy <milanilkov@gmail.com>
2025-10-03 15:22:39 +0200

    feat(devshells): add a java devshell

---
+++ devshells/java/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Java devshell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            jdk
+            gradle
+            android-studio
+          ];
+
+          shellHook = ''
+            	export JAVA_HOME=${pkgs.jdk}
+          '';
+        };
+      });
+}
